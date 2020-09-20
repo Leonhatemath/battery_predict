1786340188d81ed4159a0f968fa0952858e19cb2
wangxu <18215568074@163.com>
2020-09-20 22:04:43 +0800

    “add correct mean_soh data"

--- test_predict/dataprocess.py
+++ test_predict/dataprocess.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 
 def getdata():
     # df = pd.read_csv("../example_data/shanghai_1990-12-19_to_2019-2-28.csv")
-    df = pd.read_parquet("../data/battery_mean_soh.parquet", engine='pyarrow').dropna()
-    df = df.loc[df["device_id"] == "8a1df6a2-ca8d-4e58-803e-b408d32e83f8", :]
+    # df = pd.read_parquet("../data/battery_mean_soh.parquet", engine='pyarrow').dropna()
+    df = pd.read_parquet("../data/mean_soh.parquet", engine='pyarrow').dropna()
+    df = df.loc[df["device_id"] == "3e4a5515-fd4a-41cc-bd66-ed77f0f2ac2b", :]
     df = df.sort_values("date")
     df["date"] = df["date"].map(lambda x: x[:4] + "-" + x[4:6] + "-" + x[6:])
     df.date = pd.to_datetime(df.date, format='%Y-%m-%d')
--- test_predict/test.py
+++ test_predict/test.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df = pd.read_parquet("../data/battery_mean_soh.parquet", engine='pyarrow')
+df = pd.read_parquet("../data/mean_soh.parquet", engine='pyarrow')
 df = df.dropna()
 df.info()
-df = df.loc[df["days"] == 172, ["model", "device_id"]]
+df = df.loc[df["device_id"] == '3e4a5515-fd4a-41cc-bd66-ed77f0f2ac2b', ['date','mean_soh','std']]
 print(df[:10])
